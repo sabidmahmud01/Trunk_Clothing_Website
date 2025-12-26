633a884ab6858dbce0d1ac4d28831edea09b5f6a
SabidMahmud <sabidmahmud01@gmail.com>
2025-12-25 19:21:19 -0600

    Mongoose setup done. next: setup models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
 
 app.get("/", (req, res) => {
     res.send("Trunk API is running");
